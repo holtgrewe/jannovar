4c1046be4bf214a2697bdd8d34074a3c134cf917
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2015-10-11 11:36:14 +0200

    Fleshing out tests for SV annotation

---
+++ jannovar-cli/src/test/java/de/charite/compbio/jannovar/cmd/annotate_vcf/JannovarAnnotateVCFSymbolicInversionTest.java
@@ -0,0 +1,54 @@
+package de.charite.compbio.jannovar.cmd.annotate_vcf;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.io.Files;
+
+import de.charite.compbio.jannovar.JannovarException;
+import de.charite.compbio.jannovar.cmd.CommandLineParsingException;
+import de.charite.compbio.jannovar.cmd.HelpRequestedException;
+
+/**
+ * Run the annotate command for a symbolic inversion allele
+ */
+public class JannovarAnnotateVCFSymbolicInversionTest {
+
+	@Rule
+	public TemporaryFolder tmpFolder = new TemporaryFolder();
+
+	// path to file with the first 93 lines of hg19 RefSeq (up to "Gnomon exon 459822 459929").
+	private String pathToSmallSer = null;
+
+	@Before
+	public void setUp() throws URISyntaxException {
+		this.pathToSmallSer = this.getClass().getResource("/hg19_small.ser").toURI().getPath();
+	}
+
+	@Test
+	public void testOnSymbolicDeletionAllele() throws IOException, URISyntaxException, CommandLineParsingException,
+			HelpRequestedException, JannovarException {
+		final File outFolder = tmpFolder.newFolder();
+		final String inputFilePath = this.getClass().getResource("/symbolic_inv.vcf").toURI().getPath();
+		String[] argv = new String[] { "annotate", "-o", outFolder.toString(), pathToSmallSer, inputFilePath };
+		System.err.println(Joiner.on(" ").join(argv));
+		new AnnotateVCFCommand(argv).run();
+		File f = new File(outFolder.getAbsolutePath() + File.separator + "symbolic_inv.jv.vcf");
+		Assert.assertTrue(f.exists());
+
+		final File expectedFile = new File(this.getClass().getResource("/symbolic_inv.jv.vcf").toURI().getPath());
+		final String expected = Files.toString(expectedFile, Charsets.UTF_8);
+		final String actual = Files.toString(f, Charsets.UTF_8).replaceAll("##jannovarCommand.*", "##jannovarCommand");
+		Assert.assertEquals(expected, actual);
+	}
+
+}
